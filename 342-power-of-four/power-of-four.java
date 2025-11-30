f777672dfdabb1dc78653dabc9c8498bb0b35e77
GAURAV PRATAP SINGH <gauravpratapsingh966@gmail.com>
2025-11-30 23:11:20 +0530

    Time: 1 ms (48.53%) | Memory: 42.6 MB (17.39%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,5 +1,7 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        return n > 0 && (Math.log(n) / Math.log(4)) % 1 == 0;
+        public boolean isPowerOfFour(int n) {
+            if (n == 1) return true;
+            if (n <= 0 || n % 4 != 0) return false;
+            return isPowerOfFour(n / 4);
+        }
     }
-}
